e12e67af71c9958178b7dba7a9ab253008463998
listen <listen180@gmail.com>
2018-11-14 18:50:28 +0800

    bug fixed: file name with space

--- pdf_info.sh
+++ pdf_info.sh
@@ -19,7 +19,19 @@ echo """
 Extracting page number and file size of PDF...
 """
 
-file_list="$(ls $1/*.pdf)"
+file_path=$1/
+file_list=$1/*.pdf
+#echo $file_list
+
+
+#for f in $1/*.pdf; do
+#    echo "$f"
+#done
+
+file_num=($file_list)
+file_num=${#file_num[@]}
+
+
 f_count=0
 
 result_file_name="$1/pdf_info_result.csv"
@@ -30,34 +42,46 @@ fi
 echo "file_name, rwx_au, size, page_num" > $result_file_name
 
 
-for file in $(ls $file_list); do
-    let f_count=f_count+1
-    if [ -r $file ]; then
-       read_au="r"
+for file in $file_list; do
+    if [ ! -f "$file" ]; then
+	echo "[!Warning] file not fount: "${file}""
     else
-       read_au="-"
-    fi
-    if [ -w $file ]; then
-       write_au="w"
-    else
-       write_au="-"
-    fi
-    if [ -x $file ]; then
-       ex_au="x"
-    else
-       ex_au="-"
-    fi
-    rwx_au=$read_au$write_au$ex_au
-    
-    ## Get PDF info
-    pdf_size="$(wc -c <"$file")"
-    pdf_page="$(pdfinfo ${file} | grep Pages | awk '{print $2}')"
+        file_name="${file/$file_path/}"
+#	echo "${file_name}"
+#	perc=$(( ${f_count} / 4000 ))
+#	echo -ne "Progress: ${f_count}/${file_num} -- ${perc}% \r"
+	echo -ne "Progress: ${f_count}/${file_num} \r"
+        let f_count=f_count+1
 
-    final_result="${file}, ${rwx_au}, $pdf_size, $pdf_page"
-    echo "${final_result}" >> $result_file_name
+        if [ -r "$file" ]; then
+           read_au="r"
+        else
+           read_au="-"
+        fi
+        if [ -w "$file" ]; then
+           write_au="w"
+        else
+           write_au="-"
+        fi
+        if [ -x "$file" ]; then
+           ex_au="x"
+        else
+           ex_au="-"
+        fi
+        rwx_au=$read_au$write_au$ex_au
+        
+        ## Get PDF info
+        pdf_size="$(wc -c <"$file")"
+#        div=1024
+#        pdf_size=expr $pdf_size/$div
+        pdf_page="$(pdfinfo "$file" | grep Pages | awk '{print $2}')"
+    
+        final_result=""$file_name", ${rwx_au}, $pdf_size, $pdf_page"
+        echo "${final_result}" >> $result_file_name
+    fi
 done
 
-echo "    $f_count PDF file in this folder has been counted."
+echo "    $f_count PDF file in this folder has been used."
 
 echo """
 ... Done. 
